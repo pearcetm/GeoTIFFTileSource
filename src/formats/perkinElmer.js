d428c81f0776753e7eae82651d4c59d9d123d1bb
Avishek Biswas <avishek.biswas.1997@gmail.com>
2024-05-10 01:37:13 +0530

    fix: add attributions

--- src/formats/perkinElmer.js
+++ src/formats/perkinElmer.js
@@ -1,3 +1,36 @@
+/**
+ * PerkinElmer Vectra QPTIFF Reader
+ *
+ * This module provides a parser to organize pages of a PerkinElmer Vectra QPTIFF
+ * file into their respective channels.
+ *
+ * OME Docs
+ * https://docs.openmicroscopy.org/bio-formats/6.5.1/formats/perkinelmer-vectra-qptiff.html
+ *
+ * Download link for QPTIFF spec
+ * https://downloads.openmicroscopy.org/images/Vectra-QPTIFF/perkinelmer/PKI_Image%20Format.docx
+ */
+
+/**
+ * Parse QPTIFF channels
+ *
+ * @param images {GeoTIFFImage[]} - Array of GeoTIFFImage objects
+ * @returns {Map<string, {name: string, color: number[], images: GeoTIFFImage[]}>}
+ *
+ * @example
+ * Map(2) {
+ *  "DAPI" => {
+ *    "name": "DAPI",
+ *    "color": [0, 0, 255],
+ *    "images": [GeoTIFFImage, GeoTIFFImage, ...]
+ *   },
+ *   "Opal 570" => {
+ *    "name": "Opal 570",
+ *    "color": [255, 255, 0],
+ *    "images": [GeoTIFFImage, GeoTIFFImage, ...]
+ *   }
+ * }
+ */
 export const parsePerkinElmerChannels = (images) => {
   const channels = new Map();
 
